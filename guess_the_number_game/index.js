28be71f8ecd1e4bebc5c8e11ec954fee9891ba2f
Luka600 <158495838+Luka600@users.noreply.github.com>
2025-11-25 11:45:28 +0100

    game

--- guess_the_number_game/index.js
+++ guess_the_number_game/index.js
@@ -19,9 +19,33 @@ function checkNumber(event){
 
     let value = parseInt(inp.value);
     
-    
+    if(isNaN(value) || value<1 || value>100){
+        msg.innerHTML = "Unesi broj između 1 i 100";
+        msg.style.color = "Crimson";
+        return
+    }
+    if(value == ranNum){
+        msg.innerHTML = "Pogodak! Broj je: " + ranNum;
+        msg.style.color = "Lime";
+    }else if(value < ranNum){
+        msg.innerHTML = "Premalo!";
+        msg.style.color = "Orange";
+    }else if(value > ranNum){
+        msg.innerHTML = "Preveliko!";
+        msg.style.color = "Orange";
+        
+    }
+    tri++;
+    updateTries();
 }
 
 function newGame(){
-    location.reload();
+    /* location.reload(); */
+    ranNum = randNumb();
+    tri = 0;
+    updateTries();
+    msg.innerHTML = "Nova igra - pokušaj ponovo!"
+    msg.style.color = "Black"
+    inp.focus(); 
+    
 }
