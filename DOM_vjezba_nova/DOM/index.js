d451f96556639b224173b8f71c3732e121b01644
Luka600 <158495838+Luka600@users.noreply.github.com>
2025-12-01 17:27:53 +0100

    TEST

---
+++ DOM_vjezba_nova/DOM/index.js
@@ -0,0 +1,17 @@
+document.query
+
+document.getElementById("title").innerHTML = "Pozdrav"
+
+document.getElementById("second").style.color = "blue"
+
+document.querySelector("input").checked
+
+document.getElementsByClassName("btn").classList.add("active")
+
+document.querySelector("#list a").setAttribute("href","https://bing.com") 
+
+document.querySelectorAll("#list li")[0].textContent = "Example text" 
+
+document.querySelectorAll("#list li")[1].classList.remove("list")
+
+document.querySelectorAll("a")[1].setAttribute("href","https://yahoo.com") 
