f7d306cb679b4f567e0a4dc73c2e564b8734b266
Luka600 <158495838+Luka600@users.noreply.github.com>
2025-12-09 10:34:45 +0100

    awdafasf

--- DOM_vjezba_nova/DOM/index.js
+++ DOM_vjezba_nova/DOM/index.js
@@ -6,7 +6,7 @@ document.getElementById("second").style.color = "blue"
 
 document.querySelector("input").checked
 
-document.getElementsByClassName("btn").classList.add("active")
+document.getElementsByClassName("btn")[0].classList.add("active")
 
 document.querySelector("#list a").setAttribute("href","https://bing.com") 
 
@@ -15,3 +15,27 @@ document.querySelectorAll("#list li")[0].textContent = "Example text"
 document.querySelectorAll("#list li")[1].classList.remove("list")
 
 document.querySelectorAll("a")[1].setAttribute("href","https://yahoo.com") 
+
+var atr = document.querySelectorAll("a")[1].getAttribute("href")
+
+document.querySelectorAll("a")[0].setAttribute("title","Web stranica") 
+
+document.querySelectorAll(".list2")[1].classList.remove("list2")
+
+var lis = document.querySelectorAll("#list2 li")[2]
+
+document.getElementsByTagName("h2")[0].style.background = "yellow"
+
+document.querySelector("#list2").innerHTML = ""
+
+document.querySelector(".btn").textContent = "Kliknuto!"
+
+document.querySelectorAll("a")[0].style.color = "green"
+
+document.getElementsByClassName("list")[0] //wip
+
+document.querySelectorAll("#list2 li")[0].textContent = "Novi tekst"; 
+
+document.querySelector("input").setAttribute("checked","true")
+
+document.querySelectorAll("#list li")[1].style.background = "blue"
